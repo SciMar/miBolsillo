7b814a0a25c19f277cefe95096512c1700d00e7b
SciMar <cramirezanzola@gmail.com>
2025-10-27 17:25:11 -0500

    [FEAT] Update relations DB

---
+++ src/migrations/1761603642727-UpdateRelationsTwo.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class UpdateRelationsTwo1761603642727 implements MigrationInterface {
+    name = 'UpdateRelationsTwo1761603642727'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`budgets\` DROP FOREIGN KEY \`FK_3ece6e1292b7a86ba82145775a7\``);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`budgets\` ADD CONSTRAINT \`FK_3ece6e1292b7a86ba82145775a7\` FOREIGN KEY (\`categoryId\`) REFERENCES \`categories\`(\`id\`) ON DELETE SET NULL ON UPDATE NO ACTION`);
+    }
+
+}
