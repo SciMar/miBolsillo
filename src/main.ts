7be7e81afca07912ebf052444b9ea7cd082705ba
valeriavcgr <valeria3025gon@gmail.com>
2025-11-06 08:45:22 -0500

    [FIX] ruta main asignacion api

--- src/main.ts
+++ src/main.ts
@@ -130,7 +130,7 @@ async function bootstrap() {
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
 
-  console.log(`🚀 Servidor corriendo en http://localhost:${port}`);
+  console.log(`🚀 Servidor corriendo en http://localhost:${port}/api`);
   }
 
 bootstrap();
